0821c7a4c43553912a8ebe99c5b557ec25d58d8a
IamNaeto <egesionucharlesobimnaeto@gmail.com>
2022-11-25 11:48:29 +0100

    update responsive hamburger

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+// Hamburger script start
+const hamMenu = document.querySelector('.hamburger-menu');
+const offScreenMenu = document.querySelector('.nav-links')
+hamMenu.addEventListener('click', () =>{
+	hamMenu.classList.toggle('active');
+	offScreenMenu.classList.toggle('active')
+})
+// Hamburger script end
+
+
+// Headline clip script start
 jQuery(document).ready(function($){
 	//set animation timing
 	var animationDelay = 2500,
