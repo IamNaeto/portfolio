c2e7c4837dd283b8d6c0999c4fa634f146a12e7c
IamNaeto <egesionucharlesobimnaeto@gmail.com>
2023-03-06 00:39:42 +0100

    added scroll reveal

--- script.js
+++ script.js
@@ -12,3 +12,28 @@ window.addEventListener("scroll", function() {
 	let navbar = document.querySelector(".nav");
 	navbar.classList.toggle("scrolled", window.scrollY > 0);
   });
+
+//   Scroll Reveal Animation
+ScrollReveal().reveal('.img-sec',{ easing: 'ease-in' });
+ScrollReveal().reveal('.about-text',{ easing: 'ease-in' });
+
+ScrollReveal().reveal('.title',{ duration: 2000 });
+ScrollReveal().reveal('.card',{ interval: 200 });
+
+ScrollReveal().reveal('.project-heading',{ duration: 2000 });
+ScrollReveal().reveal('.project-subheading',{ duration: 2000 });
+ScrollReveal().reveal('.project-col',{ interval: 200 });
+
+ScrollReveal().reveal('.skills-heading',{ duration: 2000 });
+ScrollReveal().reveal('.skills-subheading',{ duration: 2000 });
+ScrollReveal().reveal('.langbox',{ interval: 200 });
+
+ScrollReveal().reveal('.says-heading',{ duration: 2000 });
+ScrollReveal().reveal('.says-subheading',{ duration: 2000 });
+ScrollReveal().reveal('.testimonial-col',{ interval: 200 });
+
+ScrollReveal().reveal('hire',{ duration: 2000 });
+ScrollReveal().reveal('.arrow-up',{ duration: 2000 });
+ScrollReveal().reveal('.foot-enquires',{ duration: 2000 });
+ScrollReveal().reveal('.socials',{ interval: 200 });
+ScrollReveal().reveal('.end',{ duration: 2000 });
