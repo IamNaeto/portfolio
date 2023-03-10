fcfde01a4d797eadd6adaffa785302d497bff7df
IamNaeto <egesionucharlesobimnaeto@gmail.com>
2023-03-10 04:40:37 +0100

    logo and scroll revel adjust

--- script.js
+++ script.js
@@ -17,22 +17,22 @@ window.addEventListener("scroll", function () {
 ScrollReveal().reveal(".img-sec", { easing: "ease-in" });
 ScrollReveal().reveal(".about-text", { easing: "ease-in" });
 
-ScrollReveal().reveal(".title", { duration: 3000 });
-ScrollReveal().reveal(".card", { interval: 300 });
+ScrollReveal().reveal(".title", { duration: 2000 });
+ScrollReveal().reveal(".card", { interval: 200 });
 
-ScrollReveal().reveal(".project-heading", { duration: 3000 });
-ScrollReveal().reveal(".project-subheading", { duration: 3000 });
-ScrollReveal().reveal(".project-col", { interval: 300 });
+ScrollReveal().reveal(".project-heading", { duration: 2000 });
+ScrollReveal().reveal(".project-subheading", { duration: 2000 });
+ScrollReveal().reveal(".project-col", { interval: 200 });
 
-ScrollReveal().reveal(".skills-heading", { duration: 3000 });
-ScrollReveal().reveal(".skills-subheading", { duration: 3000 });
-ScrollReveal().reveal(".langbox", { interval: 300 });
+ScrollReveal().reveal(".skills-heading", { duration: 2000 });
+ScrollReveal().reveal(".skills-subheading", { duration: 2000 });
+ScrollReveal().reveal(".langbox", { interval: 200 });
 
-ScrollReveal().reveal(".says-heading", { duration: 3000 });
-ScrollReveal().reveal(".says-subheading", { duration: 3000 });
-ScrollReveal().reveal(".testimonial-col", { interval: 300 });
+ScrollReveal().reveal(".says-heading", { duration: 2000 });
+ScrollReveal().reveal(".says-subheading", { duration: 2000 });
+ScrollReveal().reveal(".testimonial-col", { interval: 200 });
 ScrollReveal().reveal(".hire", { duration: 5000 });
-ScrollReveal().reveal(".arrow-up", { duration: 3000 });
-ScrollReveal().reveal(".foot-enquires", { duration: 3000 });
-ScrollReveal().reveal(".socials", { interval: 300 });
-ScrollReveal().reveal(".end", { duration: 3000 });
+ScrollReveal().reveal(".arrow-up", { duration: 2000 });
+ScrollReveal().reveal(".foot-enquires", { duration: 2000 });
+ScrollReveal().reveal(".socials", { interval: 200 });
+ScrollReveal().reveal(".end", { duration: 2000 });
